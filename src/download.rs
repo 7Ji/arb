eb5fd95952a7d9fbc60f2fe709420d529b2cf2b5
7Ji <pugokushin@gmail.com>
2023-09-29 12:07:44 +0800

    download/clone_file: simplify logic

--- src/download.rs
+++ src/download.rs
@@ -21,65 +21,64 @@ use std::{
 
 const BUFFER_SIZE: usize = 0x400000; // 4M
 
-pub(crate) fn clone_file(source: &Path, target: &Path) {
+pub(crate) fn clone_file(source: &Path, target: &Path) 
+    -> Result<(), std::io::Error> 
+{
     if target.exists() {
-        match remove_file(&target) {
-            Ok(_) => (),
-            Err(e) => {
-                eprintln!("Failed to remove file {}: {}",
-                    &target.display(), e);
-                panic!("Failed to remove existing target file");
-            },
+        if let Err(e) = remove_file(&target) {
+            eprintln!("Failed to remove file {}: {}",
+                &target.display(), e);
+            return Err(e)
         }
     }
     match hard_link(&source, &target) {
-        Ok(_) => (),
-        Err(e) => {
+        Ok(_) => return Ok(()),
+        Err(e) => 
             eprintln!("Failed to link {} to {}: {}, trying heavy copy",
-                        target.display(), source.display(), e);
-            let mut target_file = match File::create(&target) {
-                Ok(target_file) => target_file,
-                Err(e) => {
-                    eprintln!("Failed to open {} as write-only: {}",
-                                target.display(), e);
-                    panic!("Failed to open target file as write-only");
-                },
-            };
-            let mut source_file = match File::open(&source) {
-                Ok(source_file) => source_file,
-                Err(e) => {
-                    eprintln!("Failed to open {} as read-only: {}",
-                                source.display(), e);
-                    panic!("Failed to open source file as read-only");
-                },
-            };
-            let mut buffer = vec![0; BUFFER_SIZE];
-            loop {
-                let size_chunk = match
-                    source_file.read(&mut buffer) {
-                        Ok(size) => size,
-                        Err(e) => {
-                            eprintln!("Failed to read file: {}", e);
-                            panic!("Failed to read file");
-                        },
-                    };
-                if size_chunk == 0 {
-                    break
-                }
-                let chunk = &buffer[0..size_chunk];
-                match target_file.write_all(chunk) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        eprintln!(
-                            "Failed to write {} bytes into file '{}': {}",
-                            size_chunk, target.display(), e);
-                        panic!("Failed to write into target file");
-                    },
-                }
-            }
+                        target.display(), source.display(), e),
+    }
+    let mut target_file = match File::create(&target) {
+        Ok(target_file) => target_file,
+        Err(e) => {
+            eprintln!("Failed to open {} as write-only: {}",
+                        target.display(), e);
+            return Err(e)
         },
+    };
+    let mut source_file = match File::open(&source) {
+        Ok(source_file) => source_file,
+        Err(e) => {
+            eprintln!("Failed to open {} as read-only: {}",
+                        source.display(), e);
+            return Err(e)
+        },
+    };
+    let mut buffer = vec![0; BUFFER_SIZE];
+    loop {
+        let size_chunk = match
+            source_file.read(&mut buffer) {
+                Ok(size) => size,
+                Err(e) => {
+                    eprintln!("Failed to read file: {}", e);
+                    return Err(e)
+                },
+            };
+        if size_chunk == 0 {
+            break
+        }
+        let chunk = &buffer[0..size_chunk];
+        match target_file.write_all(chunk) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!(
+                    "Failed to write {} bytes into file '{}': {}",
+                    size_chunk, target.display(), e);
+                return Err(e);
+            },
+        }
     }
     println!("Cloned '{}' to '{}'", source.display(), target.display());
+    Ok(())
 }
 
 pub(crate) fn file(url: &str, path: &Path) {
