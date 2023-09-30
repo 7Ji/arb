f512b27b09057d8e92b3b8adfe06c3a2865570a3
7Ji <pugokushin@gmail.com>
2023-09-30 22:27:36 +0800

    pkgbuild/extractor_source: clear env when extracting

--- src/pkgbuild.rs
+++ src/pkgbuild.rs
@@ -421,6 +421,7 @@ fn extractor_source(actual_identity: &Identity, pkgbuild: &PKGBUILD)
     arg0.push("] /bin/bash");
     Some(actual_identity.set_command(
         Command::new("/bin/bash")
+            .env_clear()
             .arg0(&arg0)
             .arg("-ec")
             .arg(SCRIPT)
