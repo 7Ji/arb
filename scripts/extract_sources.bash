53639580c6e3373d8a758c38d23d0f9f5ce3870d
7Ji <pugokushin@gmail.com>
2023-10-03 23:42:53 +0800

    extract sources with relative links

--- scripts/extract_sources.bash
+++ scripts/extract_sources.bash
@@ -10,6 +10,25 @@ download_sources
 srcdir="${SRCDEST}"/src
 mkdir "${srcdir}"
 cd "${srcdir}"
+get_filepath() {
+    local file="$(get_filename "$1")"
+    file="../$file"
+    local proto="$(get_protocol "$1")"
+    case $proto in
+        bzr|git|hg|svn)
+            if [[ ! -d "$file" ]]; then
+                return 1
+            fi
+        ;;
+        *)
+            if [[ ! -f "$file" ]]; then
+                return 1
+            fi
+        ;;
+    esac
+    printf "%s\n" "$file"
+}
+
 extract_sources
 if [[ "$(type -t prepare)" == 'function' ]]; then
     prepare
--- src/source/extract.rs
+++ src/source/extract.rs
@@ -3,7 +3,7 @@ use std::{
         path::{
             Path,
             PathBuf,
-        }
+        }, fs::{read_dir, read_link}
     };
 
 use super::{
@@ -41,3 +41,34 @@ pub(crate) fn extract<P: AsRef<Path>>(dir: P, sources: &Vec<Source>) {
         }
     }
 }
+
+// makepkg loves abslute link, but as we use chroot, that breaks up a lot
+// pub(crate) fn fix_src_links<P: AsRef<Path>>(srcdir: P) -> Result<(), ()>{
+//     if ! srcdir.as_ref().is_dir() {
+//         eprintln!("'{}' is not a srcdir", srcdir.as_ref().display());
+//         return Err(())
+//     }
+//     let reader = read_dir(&srcdir).or_else(|e|{
+//         eprintln!("Failed to read dir '{}': {}", srcdir.as_ref().display(), e);
+//         Err(())
+//     })?;
+//     for entry in reader {
+//         let entry = entry.or_else(|e|{
+//             eprintln!("Failed to read dir entry from '{}': {}",
+//                     srcdir.as_ref().display(), e);
+//             Err(())
+//         })?;
+//         let ftype = entry.file_type().or_else(|e|{
+//             eprintln!(
+//                 "Failed to read file type form entry '{:?}': {}", entry, e);
+//             Err(())
+//         })?;
+//         if ! ftype.is_symlink() {
+//             continue
+//         }
+//         let link_path = entry.path();
+//         let link_target = read_link(&link_path).or_else(op)
+
+//     }
+//     Ok(())
+// }
