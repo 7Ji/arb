09c38ccc6f7d41536304fd49b8b13e9f051487b2
7Ji <pugokushin@gmail.com>
2023-10-19 22:07:20 +0800

    sign: split sign_pkg() from sign_pkgs()

--- src/sign.rs
+++ src/sign.rs
@@ -2,6 +2,33 @@ use std::{path::Path, fs::read_dir, process::{Command, Stdio}};
 
 use crate::identity::IdentityActual;
 
+fn sign_pkg(actual_identity: &IdentityActual, file: &Path, key: &str) 
+    -> Result<(), ()> 
+{
+    let output = match actual_identity.set_root_drop_command(
+        Command::new("/usr/bin/gpg")
+        .arg("--detach-sign")
+        .arg("--local-user")
+        .arg(key)
+        .arg(file))
+        .stdin(Stdio::null())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output() {
+            Ok(output) => output,
+            Err(e) => {
+                eprintln!("Failed to spawn child to sign pkg: {}", e);
+                return Err(())
+            },
+        };
+    if Some(0) != output.status.code() {
+        eprintln!("Bad return from gpg");
+        Err(())
+    } else {
+        Ok(())
+    }
+}
+
 pub(crate) fn sign_pkgs(actual_identity: &IdentityActual, dir: &Path, key: &str) 
     -> Result<(), ()> 
 {
@@ -25,26 +52,7 @@ pub(crate) fn sign_pkgs(actual_identity: &IdentityActual, dir: &Path, key: &str)
         if entry.ends_with(".sig") {
             continue
         }
-        let output = match actual_identity.set_root_drop_command(
-            Command::new("/usr/bin/gpg")
-            .arg("--detach-sign")
-            .arg("--local-user")
-            .arg(key)
-            .arg(&entry))
-            .stdin(Stdio::null())
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .output() {
-                Ok(output) => output,
-                Err(e) => {
-                    eprintln!("Failed to spawn child to sign pkg: {}", e);
-                    continue
-                },
-            };
-        if Some(0) != output.status.code() {
-            eprintln!("Bad return from gpg");
-            bad = true
-        }
+        if sign_pkg(actual_identity, &entry, key).is_err() { bad = true }
     }
     if bad { Err(()) } else { Ok(()) }
 }
